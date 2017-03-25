4cae0502a2ce9b1144f716f7f4c4bbbac73c783f
Adam Noel <DistantLight27@gmail.com>
2017-03-25 16:41:42 -0700

    Adding a moving cursor

--- maze_man.py
+++ maze_man.py
@@ -6,19 +6,40 @@ STDSCR.keypad(True) # Enable keypad mode to return special keys, such as the cur
 curses.noecho() # Turn off automatic echoing of keys to the screen
 curses.cbreak() # React to keys instantly, without requiring the Enter key to be pressed
 
+X_CORD = 0
+Y_CORD = 0
+
+CHARACTER = '+'
+
+def move(direction):
+    """ Update X/Y coordinates to move the character """
+    global X_CORD
+    global Y_CORD
+
+    if direction == 'KEY_LEFT':
+        X_CORD -= 1
+    elif direction == 'KEY_RIGHT':
+        X_CORD += 1
+    elif direction == 'KEY_UP':
+        Y_CORD -= 1
+    elif direction == 'KEY_DOWN':
+        Y_CORD += 1
+
+
 def main():
     """ Main entry point """
     STDSCR.clear()
     char = ''
     while 1:
-        STDSCR.addstr(char)
+        STDSCR.addstr(Y_CORD, X_CORD, CHARACTER)
         char = STDSCR.getkey()
+        move(char)
         STDSCR.clear()
 
 if __name__ == '__main__':
     try:
         main()
-    except KeyboardInterrupt:
+    except:
         # Terminate curses application
         curses.nocbreak()
         STDSCR.keypad(False)
