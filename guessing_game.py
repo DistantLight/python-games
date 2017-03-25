6687d153d19f00de11f6718a6bf744eee0bcdf97
Adam Noel <DistantLight27@gmail.com>
2017-03-25 16:53:01 -0700

    Actually making this do something

--- guessing_game.py
+++ guessing_game.py
@@ -1,37 +1,46 @@
 """ This is a guessing game. Guess the number between 1 and 9 """
-
-# from os import system
 from random import uniform
 import curses
-SCREEN = curses.initscr()
-# from getch import getch
+
+STDSCR = curses.initscr() # Initialize curses screen
+STDSCR.keypad(True) # Enable keypad mode to return special keys, such as the cursor keys
+curses.noecho() # Turn off automatic echoing of keys to the screen
+curses.cbreak() # React to keys instantly, without requiring the Enter key to be pressed
 
 def main():
     """ Main entry point """
-    secret_number = int(uniform(1, 9))
 
-    curses.noecho()
-    # curses.cbreak()
-    SCREEN.keypad(True)
 
-    SCREEN.clear()
-    SCREEN.addstr(0, 0, 'Guess a number: ')
+    STDSCR.clear()
+
+    STDSCR.addstr('Guess a number: ')
+    char = ''
+    while 1:
+        STDSCR.addstr(0, 0, 'Guess a number ... ')
+        try:
+            STDSCR.addstr(1, 0, 'You guessed %d, the secret number was %d\n' % (guess_number, secret_number))
+        except:
+            pass
+        secret_number = int(uniform(1, 9))
+        char = STDSCR.getkey()
+        STDSCR.clear()
+        try:
+            guess_number = int(char)
+        except ValueError:
+            continue
+
+
     # SCREEN.refresh()
-
-    key_input = SCREEN.getch()
-    try:
-        if key_input == ord('q'):
-            exit()
-        guess_number = int(key_input)
-    except ValueError:
-        return
-
-    SCREEN.addstr(0, 0, 'You guessed %d, the secret number was %d' % (guess_number, secret_number))
-    SCREEN.refresh()
-    if secret_number == guess_number:
-        SCREEN.addstr(0, 0, 'YOU WIN!!!')
-        exit()
+    # if secret_number == guess_number:
+    #     SCREEN.addstr(0, 0, 'YOU WIN!!!')
+    #     exit()
 
 if __name__ == '__main__':
-    while 1:
+    try:
         main()
+    except:
+        # Terminate curses application
+        curses.nocbreak()
+        STDSCR.keypad(False)
+        curses.echo()
+        curses.endwin()
